3cc756c16e6e932231b113ef1adf5223764d73d1
Anna-Liisa Saks <annaliisa.saks@gmail.com>
2021-01-13 08:52:03 +0200

    Loodud põhikontroller ja määratud vaade faili laadimisvõimalused

---
+++ app/libs/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Controller
+{
+// load model
+
+// load view
+public  function view($view, $data = array()) {
+    if(file_exists('../app/views'.$view.'.php')){
+        require_once '../app/views'.$view.'.php';
+
+    } else {
+        die('view '.$view.' does not exist');
+    }
+}
+}
