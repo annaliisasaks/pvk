802515bf6ab5e852a606b0c251cf885fb7942902
Anna-Liisa Saks <annaliisa.saks@gmail.com>
2021-01-14 11:04:53 +0200

    Loodud ja testitud uhendus andmebaasiga

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,3 +5,4 @@ require_once 'config/constants.php';
 // load libs
 require_once 'libs/core.php';
 require_once 'libs/Controller.php';
+require_once 'libs/Database.php';
--- app/config/constants.php
+++ app/config/constants.php
@@ -6,4 +6,8 @@ define('APPROOT', dirname(dirname(__FILE__)));
 // url root
 define('URLROOT','http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
 
-
+// parameetrid db
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'pvk');
