c57bc50571a9398bdc946e746f489e28ccd56b80
Anna-Liisa Saks <annaliisa.saks@gmail.com>
2021-01-14 16:21:53 +0200

    Andmete lugemine

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -13,17 +13,23 @@ class Pages extends Controller
     }
 
     public function index(){
-      //  echo 'index method is loaded<br>';
+        // echo 'index method is loaded<br>';
         $data = array(
-            'title'=>'Welcome to pages',
-            'content' =>  'Pages index vies is loaded'
+            'title' => 'Welcome to Pages',
+            'content' => 'Pages index view is loaded by MVC'
         );
         $this->view('pages/index', $data);
     }
-    public function about(){
-      //  echo 'about method is loaded<br>';
-        $data = array('content'=>'Pages about view is loaded by MVC');
-        $this->view('pages/about', $data);
 
+    public function about(){
+        // model test
+        // $this->pagesModel->addUser();
+        // $users = $this->pagesModel->getUsers();
+        // print_r($users);
+        $usersCount = $this->pagesModel->usersCount();
+        echo 'Users in db - '.$usersCount.'<br>';
+        // echo 'about method is loaded<br>';
+        $data = array('content' => 'Pages about view is loaded by MVC');
+        $this->view('pages/about', $data);
     }
 }
